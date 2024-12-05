fe9e1f6333a0a3de7d03da71aa9ff29fef1c02de
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-12-05 17:02:35 -0300

    Add typewriter effect on logo

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,32 @@
-let titleArea = document.querySelector('#titleArea');
+let logoArea = document.querySelector('#logoArea');
 
-var typewriter = new Typewriter(titleArea, {
-    loop: true,
+var typewriterLogo = new Typewriter(logoArea, {
+    loop: false,
     delay: 75,
 });
 
-typewriter
+typewriterLogo
+    .typeString('.dev')
+    .pauseFor(1000)
+    .typeString(': Portfólio')
+    .pauseFor(4000)
+    .deleteChars(14)
+    .typeString('dev')
+    .start();
+
+let titleArea = document.querySelector('#titleArea');
+
+var typewriterTitle = new Typewriter(titleArea, {
+    loop: false,
+    delay: 75,
+});
+
+typewriterTitle
     .typeString('Desenvolvedor Front-end')
     .pauseFor(2500)
     .deleteChars(23)
     .typeString('Analista de Suporte')
     .pauseFor(1000)
+    .deleteChars(19)
+    .typeString('Desenvolvedor Front-end')
     .start();
