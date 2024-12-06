838a806dd1085840e01ffa61caba7df8c42fd25e
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-12-06 08:23:37 -0300

    Add authorization lix API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ async function getLinkedinInformation() {
     let req = await fetch(`https://api.lix-it.com/v1/person?profile_link=${linkedinProfile}`, { 
         method: 'GET', 
         headers: {
-            'Access-Control-Allows-Origin': '',
+            'Access-Control-Allows-Origin': 'https://portfolio-njuqpaezf-caiohluwis-projects.vercel.app/',
+            'Access-Control-ALlow-Method': 'GET',
+            'Authorization': 'SSDBwtExzWQvFRtczfAGtPOp0KV27TL0JqU2Hth5VIXZIzy3BwhCv6mRI6mL',
             'Accept': 'application/json'
         }
     });
