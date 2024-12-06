b97ac0232c26425cc71cf42c2d3f3544aa2da9f4
Caio Henrique <caiohenriquelw2004@gmail.com>
2024-12-06 08:31:54 -0300

    Try to fix requisition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 async function getLinkedinInformation() {
-    const linkedinProfile = 'https://www.linkedin.com/in/caiohenriquelw'
+    const linkedinProfile = 'https://www.linkedin.com/in/caiohenriquelw';
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer SSDBwtExzWQvFRtczfAGtPOp0KV27TL0JqU2Hth5VIXZIzy3BwhCv6mRI6mL`,
+    }
 
     let req = await fetch(`https://api.lix-it.com/v1/person?profile_link=${linkedinProfile}`, { 
-        method: 'GET', 
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'SSDBwtExzWQvFRtczfAGtPOp0KV27TL0JqU2Hth5VIXZIzy3BwhCv6mRI6mL',
-        }
+        method: 'GET',
+        headers
     });
     let response  = await req.json();
     response.catch((err) => {
